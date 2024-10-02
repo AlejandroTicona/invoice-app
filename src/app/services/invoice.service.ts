119d83f6389bbfd46184d175971da1d01ec6ac4b
Aletdev <alejandrortsinani@outlook.com>
2024-10-02 18:14:38 -0400

    Uso de bootstrap en invoice.component.html

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -5,7 +5,10 @@ import { invoiceData } from '../data/invoice.data';
 @Injectable({
   providedIn: 'root'
 })
+//aqui se tendran solo implementaciones y consultas a A.P.I.s 
+//http, el service es para eso, para trabajar con servicios rest y apis
 export class InvoiceService {
+
 
   //obteniendo datos de manera hardcodeada, o directamente del codigo
   private invoice: Invoice = invoiceData;
